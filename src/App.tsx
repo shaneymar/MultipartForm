4e78eeb06434bee0b5d3c6dca2add489e76d985b
shankar saravanan <b.s.shankar31@gmail.com>
2025-10-25 19:00:24 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button className="cursor-pointer">Click me</Button>
+    </div>
+  )
+}
+
+export default App
